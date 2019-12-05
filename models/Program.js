07260313038b1c81ad068eea938338e0391c058e
RobinTheSprite <madscientistak@gmail.com>
2019-12-05 11:03:37 -0900

    Fix program schema

--- models/Program.js
+++ models/Program.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const ProgramSchema = mongoose.Schema({
    name: {type: String, trim: true},
-   workers: {type: [mongoose.Schema.ObjectID]},
-   supervisor: {type: mongoose.Schema.ObjectID}
+   workers: {type: [mongoose.Schema.ObjectId]},
+   supervisor: {type: mongoose.Schema.ObjectId}
 });
 
-module.exports = mongoose.Model('Program', ProgramSchema);
+module.exports = mongoose.model('Program', ProgramSchema);
