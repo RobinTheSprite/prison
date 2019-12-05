f7a93c366169abd29247f58ea937bc03a14016b4
RobinTheSprite <madscientistak@gmail.com>
2019-12-05 10:53:45 -0900

    Add program table fetcher

---
+++ public/scripts/getPrograms.js
@@ -0,0 +1,57 @@
+function addCell(tr, val) {
+    var td = document.createElement('td');
+
+    if (Array.isArray(val))
+    {
+        td.style.overflow = 'auto';
+        val.forEach((item) => {
+            delete item._id;
+            if (typeof item === 'object')
+            {
+                Object.values(item).forEach(itemVal => {
+                    td.innerHTML += itemVal + ' ';
+                });
+
+            }
+            else
+            {
+                td.innerHTML += item;
+            }
+            td.innerHTML += '<br/>';
+        })
+    }
+    else
+    {
+        td.innerHTML = val;
+    }
+
+    tr.appendChild(td)
+}
+
+function addRow(tbl, data) {
+    var tr = document.createElement('tr');
+
+    addCell(tr, data.name);
+    addCell(tr, data.supervisor);
+    addCell(tr, data.workers);
+
+    tbl.appendChild(tr)
+}
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'http://localhost:3000/api/program', true);
+
+request.onload = function () {
+    const response = JSON.parse(this.response);
+    response.data.forEach((row) => {
+        addRow(document.getElementById('programTable'), row);
+    })
+};
+
+request.responseType = 'text';
+
+// Send request
+request.send();
