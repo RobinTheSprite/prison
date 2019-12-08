f88f5ee728c8c16a36cd7bafcf1b274c44f51dfa
Mark Underwood <mark.t.underwood@protonmail.com>
2019-12-07 16:44:43 -0900

    Make name an index

--- models/Program.js
+++ models/Program.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ProgramSchema = new mongoose.Schema({
-   name: {type: String, trim: true},
+   name: {type: String, trim: true, index: true},
    workers: {type: [String]},
    supervisor: {type: String}
 });
